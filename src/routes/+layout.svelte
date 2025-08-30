0b97d4f42eef928a33be7bbc6c627ca03bc25f59
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2025-08-30 10:23:05 -0400

    feat: add lenis

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	import "../app.css";
+	import "lenis/dist/lenis.css";
 	import favicon from "$lib/assets/favicon.svg";
 	import ScrollProgress from "$lib/components/ScrollProgress.svelte";
+	import Lenis from "lenis";
+	import { onMount } from "svelte";
+	import { frame } from "motion";
 
 	let { children } = $props();
+
+	onMount(() => {
+		const lenis = new Lenis();
+		frame.render((d) => lenis.raf(d.timestamp), true);
+
+		return () => lenis.destroy();
+	});
 </script>
 
 <svelte:head>
