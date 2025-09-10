4ccfe867ef332a5dfdb963e3a777b745f793a6fc
Oliver Rose <hello@olivermrose.com>
2025-09-10 00:52:25 -0400

    chore(web): analytics/speed insights

--- apps/web/src/routes/+layout.server.ts
+++ apps/web/src/routes/+layout.server.ts
@@ -1,5 +1,10 @@
+import { injectAnalytics } from "@vercel/analytics/sveltekit";
+import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
 import { PUBLIC_CMS_API_URL } from "$env/static/public";
 import { payloadResponse, postSchema } from "$lib/schema";
+
+injectAnalytics();
+injectSpeedInsights();
 
 export async function load({ fetch }) {
 	const postsResponse = await fetch(
