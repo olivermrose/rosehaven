0c4152b5b127dee1f4185cf8fccaaee5731cfdcd
Oliver Rose <hello@olivermrose.com>
2025-09-12 15:06:32 -0400

    refactor(web): always use motion

--- apps/web/src/lib/components/ScrollProgress.svelte
+++ apps/web/src/lib/components/ScrollProgress.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
 	import { animate, scroll } from "motion";
 	import { onMount } from "svelte";
-	import { page } from "$app/state";
 
 	onMount(() => {
 		animate(".scroll-progress", { opacity: 1 }, { duration: 0.5 });
-	});
 
-	$effect(() => {
-		if (CSS.supports("animation-timeline: scroll()")) return;
-		void page.url;
-
-		scroll(
+		const cancel = scroll(
 			animate([
 				[".left", { scaleX: [0, 1] }, { at: 0 }],
 				[".right", { scaleX: [1, 0] }, { ease: [0.42, 0, 0.58, 1], at: 0 }],
 			]),
 		);
+
+		return () => cancel();
 	});
 </script>
 
@@ -32,50 +28,3 @@
 		class="right bg-plum-600 absolute right-0 h-full w-1/5 origin-right dark:bg-neutral-500"
 	></div>
 </div>
-
-<style>
-	@supports (animation-timeline: scroll()) {
-		:global(html) {
-			animation: detect-scroll linear;
-			animation-timeline: scroll(self);
-		}
-
-		.left {
-			opacity: calc(1 * var(--has-scroll, 0));
-			animation: grow forwards;
-			animation-timeline: scroll(root y);
-		}
-
-		.right {
-			animation: shrink cubic-bezier(0.42, 0, 0.58, 1) forwards;
-			animation-timeline: scroll(root y);
-		}
-
-		@keyframes detect-scroll {
-			from,
-			to {
-				--has-scroll: 1;
-			}
-		}
-
-		@keyframes grow {
-			from {
-				transform: scaleX(0);
-			}
-
-			to {
-				transform: scaleX(1);
-			}
-		}
-
-		@keyframes shrink {
-			from {
-				transform: scaleX(1);
-			}
-
-			to {
-				transform: scaleX(0);
-			}
-		}
-	}
-</style>
