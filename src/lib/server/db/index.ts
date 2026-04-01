16fd2a04259bc149a3ed0b7c7c90782300f56029
Oliver Rose <hello@olivermrose.com>
2026-03-31 23:28:07 -0400

    fix: pass conn str directly

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,4 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
 import { env } from "$env/dynamic/private";
-import * as schema from "./schema";
 
-const client = postgres(env.DATABASE_URL);
-export const db = drizzle(client, { schema });
+export const db = drizzle(env.DATABASE_URL);
