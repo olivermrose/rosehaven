e70c921a43f6344651fb7d28a03631f164d2b840
Oliver Rose <hello@olivermrose.com>
2026-04-02 20:24:39 -0400

    refactor: use inset anchor

--- src/lib/components/Anthology.svelte
+++ src/lib/components/Anthology.svelte
@@ -47,36 +47,36 @@
 
 					<ul class="focus grid grid-cols-2 gap-8 sm:grid-cols-3 lg:grid-cols-4">
 						{#each posts as post (post.id)}
-							<li>
-								<a
-									class="-m-4 flex flex-col p-4"
-									title={post.title + (post.explicit ? " (Explicit)" : "")}
-									href="/writing/{post.slug}"
-								>
-									<div class="flex items-center gap-x-2">
-										<h3 class="line-clamp-2 text-fluid-base font-medium">
-											{post.title}
-										</h3>
+							{@const title = post.title + (post.explicit ? " (Explicit)" : "")}
 
-										{#if post.explicit}
-											<svg class="size-5 opacity-30" width="32" height="32" viewBox="0 0 24 24">
-												<path
-													fill="currentColor"
-													d="M9 17h6v-2h-4v-2h4v-2h-4V9h4V7H9zm-4 4q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21z"
-												/>
-											</svg>
-										{/if}
-
-										{#if newPosts.has(post.id)}
-											<span
-												class="inline-block size-2 shrink-0 rounded-full bg-peach-400"
-												aria-label="New"
-											></span>
-										{/if}
-									</div>
-
-									<p class="mt-1 text-fluid-xs">{post.excerpt}</p>
+							<li class="relative -m-4 flex flex-col p-4">
+								<a class="absolute inset-0" {title} href="/writing/{post.slug}">
+									<span class="sr-only">{title}</span>
 								</a>
+
+								<div class="flex items-center gap-x-2">
+									<h3 class="line-clamp-2 text-fluid-base font-medium">
+										{post.title}
+									</h3>
+
+									{#if post.explicit}
+										<svg class="size-5 opacity-30" width="32" height="32" viewBox="0 0 24 24">
+											<path
+												fill="currentColor"
+												d="M9 17h6v-2h-4v-2h4v-2h-4V9h4V7H9zm-4 4q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21z"
+											/>
+										</svg>
+									{/if}
+
+									{#if newPosts.has(post.id)}
+										<span
+											class="inline-block size-2 shrink-0 rounded-full bg-peach-400"
+											aria-label="New"
+										></span>
+									{/if}
+								</div>
+
+								<p class="mt-1 text-fluid-xs">{post.excerpt}</p>
 							</li>
 						{/each}
 					</ul>
