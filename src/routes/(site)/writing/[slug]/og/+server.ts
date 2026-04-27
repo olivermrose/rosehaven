73acc0d981a6e368c8015e0c63127a367ab2a634
Oliver Rose <hello@olivermrose.com>
2026-04-26 21:03:33 -0400

    feat: upgrade to takumi v1

--- src/routes/(site)/writing/[slug]/og/+server.ts
+++ src/routes/(site)/writing/[slug]/og/+server.ts
@@ -1,8 +1,10 @@
-import { ImageResponse } from "@takumi-rs/image-response/wasm";
+import { ImageResponse } from "takumi-js/response";
 import { eq } from "drizzle-orm";
 import { dev } from "$app/environment";
 import { posts } from "$lib/server/db/schema";
-import { template } from "./template";
+import { render } from "svelte/server";
+import Template from "./Template.svelte";
+import styles from "../../../../../app.css?inline";
 
 export async function GET({ params, locals }) {
 	const [post] = await locals.db
@@ -10,21 +12,23 @@ export async function GET({ params, locals }) {
 		.from(posts)
 		.where(eq(posts.slug, params.slug));
 
-	return new ImageResponse(
-		template({
+	const { body, head } = await render(Template, {
+		props: {
 			title: post.title,
 			excerpt: post.excerpt,
-		}),
-		{
-			width: 1200,
-			height: 630,
-			format: "webp",
-			headers: {
-				// 31536000 = 365 days
-				"Cache-Control": dev
-					? "no-cache, no-store"
-					: "public, immutable, no-transform, max-age=31536000",
-			},
 		},
-	);
+	});
+
+	return new ImageResponse(head + body, {
+		stylesheets: [styles],
+		width: 1200,
+		height: 630,
+		format: "webp",
+		headers: {
+			// 31536000 = 365 days
+			"Cache-Control": dev
+				? "no-cache, no-store"
+				: "public, immutable, no-transform, max-age=31536000",
+		},
+	});
 }
---
+++ src/routes/(site)/writing/[slug]/og/Template.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { BASE_URL } from "$lib";
+
+	const colors = [
+		"#9e3340", // wine
+		"#8daa5a", // aloe
+		"#e49367", // peach
+		"#5c86b2", // denim
+		"#9b4672", // plum
+	];
+
+	interface Props {
+		title: string;
+		excerpt: string;
+	}
+
+	const { title, excerpt }: Props = $props();
+</script>
+
+<div class="relative flex h-full w-full flex-col bg-white">
+	<div class="flex flex-col justify-between p-10">
+		<div class="-mt-2 flex items-center">
+			<!-- svelte-ignore a11y_missing_attribute -->
+			<img class="size-14 invert" src="{BASE_URL}/favicon.svg" />
+
+			<h1 class="ml-4 text-4xl font-normal">oliver rose.</h1>
+		</div>
+
+		<hgroup class="flex w-full flex-col">
+			<h2 class="mb-0 flex text-8xl/none font-bold">{title}</h2>
+
+			<p class="flex max-w-3/4 text-4xl text-neutral-600">
+				{excerpt}
+			</p>
+		</hgroup>
+	</div>
+
+	<div class="absolute bottom-0 flex h-4 w-full">
+		{#each colors as color}
+			<span class="flex h-full grow" style="background-color: {color}"></span>
+		{/each}
+	</div>
+</div>
